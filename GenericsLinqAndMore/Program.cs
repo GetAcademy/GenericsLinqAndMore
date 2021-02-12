62b90b22395c4df15123aba28f82d3969bed92b4
GET Academy <terje@kolderup.net>
2021-02-12 09:19:26 +0100

    Initial commit

---
+++ GenericsLinqAndMore/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericsLinqAndMore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * LinkedListInt
+             * LinkedList med generics
+             * BuilderPattern (og hva er design patterns)
+             *
+             *
+             *
+             */
+        }
+    }
+}
